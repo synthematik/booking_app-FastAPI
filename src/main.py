249d764011d87b62e9c745b5586bc4d6927e2eaa
Garik <garfild_2003@mail.ru>
2023-12-19 17:51:04 +0300

    Updated

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
 
-app = FastAPI()
+from fastapi import FastAPI, Query
+
+app = FastAPI(debug=True)
 
 
 @app.get("/")
@@ -8,6 +11,17 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/hotel/{hotel_id}/")  # {} - параметры пути
+async def get_hotel(hotel_id: int, date_from, date_to):  # параметры запроса
+    return {"hotel_id": hotel_id, "date_from": date_from, "date_to": date_to}
+
+
+@app.get("/hotels/")  # {} - параметры пути
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,  # Опциональные аргументы, то есть могут быть нулевыми
+        stars: Optional[int] = Query(None, ge=1, le=5),  # Ограничение кол-ва звезд от 1 до 5
+):
+    return {"location": location, "date_from": date_from, "date_to": date_to, "stars": stars, "has_spa": has_spa}
