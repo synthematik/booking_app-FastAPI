0a55f646e9ea2f05c9f0ec0bdc0a1786f01dde58
Garik <garfild_2003@mail.ru>
2023-12-22 19:19:53 +0300

    Room model has been moved to hotel folder

--- src/hotels/models.py
+++ src/hotels/models.py
@@ -1,5 +1,5 @@
 from src.database import Base
-from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 
 class Hotel(Base):
@@ -13,3 +13,15 @@ class Hotel(Base):
     image_id = Column("image_id", Integer)
     stars = Column("stars", Integer, nullable=False)
 
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column("id", Integer, primary_key=True)
+    hotel_id = Column("hotel_id", ForeignKey("hotels.id"))
+    name = Column("name", String)
+    description = Column("description", String)
+    price = Column("price", Integer)
+    services = Column("services", JSON)
+    quantity = Column("quantity", Integer)
+    image_id = Column("image_id", Integer)
