1aaef5af7f2b4274029c81f7250be81a0ba84de4
Garik <garfild_2003@mail.ru>
2023-12-31 18:26:31 +0300

    Added an endpoint to get a list of hotels with available rooms by location

---
+++ src/hotels/router.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+from fastapi import APIRouter
+
+from src.hotels.service import HotelService
+from src.hotels.schemas import SHotelInfo
+from src.exception import DateFromCannotBeAfterDateTo, CannotBookHotelForLongTime
+
+
+router = APIRouter(
+    prefix="/hotels",
+    tags=["Hotels"]
+)
+
+
+@router.get("/{location}/")
+async def get_hotels_by_location(
+        location: str, date_from: date, date_to: date
+) -> list[SHotelInfo]:
+    if date_from > date_to:
+        raise DateFromCannotBeAfterDateTo
+    if (date_to - date_from).days > 31:
+        raise CannotBookHotelForLongTime
+    hotels = await HotelService.find_all_hotels(location, date_from, date_to)
+    return hotels
+
+
+
+
