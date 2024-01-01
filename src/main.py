4e139c501d33effc395ad38be6a68ce07233cbdc
Garik <garfild_2003@mail.ru>
2024-01-01 20:25:02 +0300

    Added CORS

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.bookings.router import router as bookings_router
 from src.users.router import router as users_router
 from src.hotels.router import router as hotels_router
 from src.hotels.rooms.router import router as rooms_router
+from src.pages.router import router as pages_router
+from src.files.router import router as files_router
 
 
 app = FastAPI(debug=True)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(users_router)
 
@@ -16,7 +22,19 @@ app.include_router(hotels_router)
 
 app.include_router(rooms_router)
 
+app.include_router(pages_router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(files_router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATH", "OPTIONS"],
+    allow_headers=["*"],
+)
