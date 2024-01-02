1e59f5cf252176d4cc18594391d2dda084baa69d
Garik <garfild_2003@mail.ru>
2024-01-02 17:42:35 +0300

    Added html to some endpoint

--- src/main.py
+++ src/main.py
@@ -27,8 +27,9 @@ app.include_router(pages_router)
 app.include_router(files_router)
 
 origins = [
-    "http://localhost",
+    "http://localhost:8000",
     "http://localhost:8080",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
--- src/pages/router.py
+++ src/pages/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from src.hotels.router import get_hotels_by_location, get_all_hotels
 
@@ -18,7 +19,22 @@ async def root(request: Request):
     return templates.TemplateResponse("base.html", {"request": request})
 
 
+@router.get("/login/", name='login', response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@router.get("/register/")
+async def register(request: Request):
+    return templates.TemplateResponse("auth/register.html", {"request": request})
+
+
 @router.get("/hotels/", name='hotels_list')
 async def get_hotels_page(request: Request, hotels=Depends(get_all_hotels)):
-    return templates.TemplateResponse("hotels.html", {"request": request, "hotels": hotels})
+    return templates.TemplateResponse("hotels/hotels.html", {"request": request, "hotels": hotels})
+
+
+@router.get("/{location}/")
+async def get_hotels_by_location_page(request: Request, hotels=Depends(get_hotels_by_location)):
+    return templates.TemplateResponse("hotels/hotels_location.html", {"request": request, "hotels": hotels})
 
