9176393b93a70935db59315e8bdca9c552f64381
Garik <garfild_2003@mail.ru>
2024-01-04 17:12:11 +0300

    Bugfix

--- src/pages/router.py
+++ src/pages/router.py
@@ -16,17 +16,17 @@ templates = Jinja2Templates(directory="templates/")
 
 @router.get("/", name='root')
 async def root(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+    return templates.TemplateResponse("base_app.html", {"request": request})
 
 
 @router.get("/login/", name='login', response_class=HTMLResponse)
 async def login(request: Request):
-    return templates.TemplateResponse("auth/login.html", {"request": request})
+    return templates.TemplateResponse("auth/login_user.html", {"request": request})
 
 
 @router.get("/register/")
 async def register(request: Request):
-    return templates.TemplateResponse("auth/register.html", {"request": request})
+    return templates.TemplateResponse("auth/register_user.html", {"request": request})
 
 
 @router.get("/hotels/", name='hotels_list')
