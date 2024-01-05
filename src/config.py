fb32b2e9c292a17015e9ab0c5db4dacbc6c14252
Garik <garfild_2003@mail.ru>
2024-01-05 16:10:40 +0300

    An environment has been prepared for further test writing

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings
 
 
@@ -7,6 +9,12 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
 
     ALGORITHM: str
     SECRET_KEY: str
@@ -19,8 +27,13 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
 
+    MODE: Literal["DEV", "PROD", "TEST"]
+
     def get_database_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    def get_test_database_url(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
     class Config:
         env_file = ".env"
--- src/database.py
+++ src/database.py
@@ -1,13 +1,16 @@
-from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from src.config import settings
+from sqlalchemy import NullPool
 
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.get_test_database_url()
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.get_database_url()
+    DATABASE_PARAMS = {}
 
-DATABASE_URL = settings.get_database_url()
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
